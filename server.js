0f706261a6ade5dae0a0010151e3a44b9fe1cd62
Enoch <enochaniyikaye@gmail.com>
2024-02-14 11:54:01 +0100

    i just added the products page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
